bb881fd48c1a7f8ac9420988590b285e00804867
iaza18 <zyamka1808@gmail.com>
2023-01-05 00:35:51 +0200

    Added Dictionary component and search form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Dictionary from './Dictionary';
 
 export default function App() {
   return (
     <div className="App">
-      <h1 className="App-header">Dictionary</h1>
+      <div className="container">
+        <main>
+          <h1> Dictionary</h1>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          Coded by Iana Zavoloka
+        </footer>
+      </div>
     </div>
   );
 }
