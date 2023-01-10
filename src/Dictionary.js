636000caa841fe84af6e0128268e8d2235ca466b
iaza18 <zyamka1808@gmail.com>
2023-01-10 18:20:38 +0200

    Rendered the word meaning

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,24 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results.js";
 import "./Dictionary.css";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
 
 function handleResponse(response){
     console.log(response.data[0]);
+    setResults(response.data[0]);
+    //console.log(response.data[0].meanings[0].definitions[0].definition);
+
 }
 
     function search(event){
         event.preventDefault();
 
         //documentation: https://dictionaryapi.dev/
-        
+
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
     }
@@ -31,7 +36,7 @@ function handleResponse(response){
             placeholder="Search"
             onChange={handleKeywordChange} />
         </form>
-
+        <Results results={results} />
     </div>
     );  
 }
