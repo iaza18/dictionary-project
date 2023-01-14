4c5efe828f0097dc40719b24365791728ab8efc2
iaza18 <zyamka1808@gmail.com>
2023-01-14 21:55:06 +0200

    Added Phonetic component

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -3,14 +3,12 @@ import React from "react";
 export default function Synonyms(props) {
    if (props.synonyms.length !== 0) {
     return (
-        <div className="Synonyms">
+        <ul className="Synonyms">
             <hr />
-            <ul>
         {props.synonyms.map(function (synonym, index) {
             return <li key={index}>{synonym}</li>;
         })}
         </ul>
-        </div>
     );
    } else {
        return null; 
